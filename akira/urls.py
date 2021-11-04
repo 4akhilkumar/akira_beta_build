73d77f7526edb154a96a49e004949ae6638450ff
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-11-04 21:42:25 +0530

    serve media files

--- akira/urls.py
+++ akira/urls.py
@@ -18,7 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+
 from django.contrib.auth.decorators import login_required
+from django.urls.conf import re_path
 from django.views.decorators.cache import never_cache
 from ckeditor_uploader import views as ckeditor_views
 
@@ -34,7 +37,5 @@ urlpatterns = [
     path('ckeditor/upload/', login_required(ckeditor_views.upload), name='ckeditor_upload'),
     path('ckeditor/browse/', never_cache(login_required(ckeditor_views.browse)), name='ckeditor_browse'),
 
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root':settings.MEDIA_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
