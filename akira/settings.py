9e91252607ded9687b4c6f9e89ff2a2d373c47f2
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-01-30 11:44:21 +0530

    MySQL DB is shutdown. Default DB is online

--- akira/settings.py
+++ akira/settings.py
@@ -111,27 +111,27 @@ WSGI_APPLICATION = 'akira.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-#Change the default database to MYSQL Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'AkirADB',
-        'HOST': 'database-2.cuu7bf6xisq0.us-east-1.rds.amazonaws.com',
-        'PORT': '3306',
-        'USER': 'admin',
-        'PASSWORD': 'AWSDBaccount21',
-        'OPTIONS': {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# #Change the default database to MYSQL Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'AkirADB',
+#         'HOST': 'database-2.cuu7bf6xisq0.us-east-1.rds.amazonaws.com',
+#         'PORT': '3306',
+#         'USER': 'admin',
+#         'PASSWORD': 'AWSDBaccount21',
+#         'OPTIONS': {
+#             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+#         }
+#     }
+# }
 
 
 # Password validation
